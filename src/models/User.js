9b6abee712b07b37ebb56e499a4cbb70cbcfa222
Javier Gaspart <javiergaspart@gmail.com>
2025-05-09 17:23:42 +0530

    Update backend with new MongoDB and .env setup

--- src/models/User.js
+++ src/models/User.js
@@ -11,8 +11,8 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
+    required: false,
+    unique: false,
   },
   phone: {
     type: String,
