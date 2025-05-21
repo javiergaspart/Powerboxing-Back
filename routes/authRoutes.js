1d0f5509acb758af6746be96513e0546ee3ac745
Javier Gaspart <javiergaspart@gmail.com>
2025-05-21 17:36:55 +0530

    Fix: Correct login function name in authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,11 +5,11 @@ const authController = require('../controllers/authController');
 const otpController = require('../controllers/otpController');
 
 // OTP
-// router.post('/send-otp', otpController.sendOtpSignup);
-// router.post('/verify-otp', otpController.verifyOtpSignup);
+router.post('/send-otp', otpController.sendOtpSignup);
+router.post('/verify-otp', otpController.verifyOtpSignup);
 
 // Login
-router.post('/login', authController.login);
+router.post('/login', authController.userLogin);
 router.post('/trainer-login', authController.trainerLogin);
 
 module.exports = router;
