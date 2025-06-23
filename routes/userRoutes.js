19d951e2686d2f97a1aa18e2b3be13dabc6c3f4a
Javier Gaspart <javiergaspart@gmail.com>
2025-06-23 22:37:16 +0100

    Backend updates

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,6 +10,5 @@ router.get('/', userController.getAllUsers);
 router.post('/upload-profile-image/:id', userController.upload.single('image'), userController.uploadProfileImage);
 router.put('/updateProfile/:id', userController.updateProfile);
 router.post('/direct-signup', userController.directSignUp);
-router.get('/users/:id', userController.getUserById);
 
 module.exports = router;
